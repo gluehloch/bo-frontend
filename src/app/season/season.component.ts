dfcaf39356b57bd794494273bca63dc3385b5376
Andre Winkler <andre.winkler@web.de>
2017-02-12 13:35:19 +0100

    Under construction: Find all seasons and show a selection

--- src/app/season/season.component.ts
+++ src/app/season/season.component.ts
@@ -2,11 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 import { USERROLE } from '../user-role.enum';
 
-@Component({
-  selector: 'season',
-  templateUrl: './season.component.html',
-  styleUrls: ['./season.component.css']
-})
+import { SeasonService} from './season.service';
 
 export class Roundtable {
   seasons: Rest.SeasonJson[];
@@ -15,14 +11,46 @@ export class Roundtable {
   selectedGroup: Rest.GroupTypeJson[];
   rounds: Rest.RoundJson[];
   selectedRound: Rest.RoundJson;
-  table: string /* TODO Array!*/
+  table: Rest.RoundAndTableJson[];
 };
 
+@Component({
+  selector: 'season',
+  templateUrl: './season.component.html',
+  styleUrls: ['./season.component.css']
+})
 export class SeasonComponent implements OnInit {
 
-  constructor() { }
+  roundtable: Roundtable;
 
-  ngOnInit() {
+  constructor(private seasonService: SeasonService) {
+    this.roundtable = new Roundtable();
+    this.roundtable.seasons = new Array();
   }
 
+  ngOnInit() {
+    this.findSeasons();
+  }
+
+  findSeasons() {
+    this.seasonService.findSeasons().subscribe((seasons: Rest.SeasonJson[]) =>
+      this.roundtable.seasons = seasons
+    );
+  }
+
+  /*
+    var callbackSeasons = function(seasons) {
+        $scope.roundtable.seasons = seasons;
+        $scope.roundtable.groups = [];
+        $scope.roundtable.selectedGroup = null;
+        $scope.roundtable.rounds = [];
+        $scope.roundtable.selectedRound = null;
+        $scope.roundtable.table = [];
+
+        if (seasons !== null && seasons.length > 0) {
+            $scope.roundtable.selectedSeason = seasons[seasons.length - 1].id;
+            $scope.seasonSelected();
+        }
+    };
+  */
 }
