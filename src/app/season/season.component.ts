bf5dff4058044c89684668d1a2a75086df7b5a73
andrewinkler <andre.winkler@web.de>
2017-02-14 18:33:01 +0100

    Under construction: the event handling is not so great for my season selector.

--- src/app/season/season.component.ts
+++ src/app/season/season.component.ts
@@ -8,7 +8,7 @@ export class Roundtable {
   seasons: Rest.SeasonJson[];
   selectedSeason: Rest.SeasonJson;
   groups: Rest.GroupTypeJson[];
-  selectedGroup: Rest.GroupTypeJson[];
+  selectedGroup: Rest.GroupTypeJson;
   rounds: Rest.RoundJson[];
   selectedRound: Rest.RoundJson;
   table: Rest.RoundAndTableJson[];
@@ -25,7 +25,7 @@ export class SeasonComponent implements OnInit {
 
   constructor(private seasonService: SeasonService) {
     this.roundtable = new Roundtable();
-    this.roundtable.seasons = new Array();
+    //this.roundtable.seasons = new Array();
   }
 
   ngOnInit() {
@@ -34,16 +34,33 @@ export class SeasonComponent implements OnInit {
 
   findSeasons() {
     this.seasonService.findSeasons()
-                      .subscribe((seasons: Rest.SeasonJson[]) =>
-      this.roundtable.seasons = seasons
-    );
+                      .subscribe((seasons: Rest.SeasonJson[]) => {
+      this.roundtable.seasons = seasons;
+      if (this.roundtable.seasons && this.roundtable.seasons.length > 0) {
+        this.findGroups();
+      }
+    });
   }
 
-  findGroup() {
+  findGroups() {
     this.seasonService.findGroups(this.roundtable.selectedSeason.id)
-                      .subscribe((groups: Rest.GroupTypeJson[]) =>
-      this.roundtable.groups = groups
-    );
+                      .subscribe((groups: Rest.GroupTypeJson[]) => {
+      this.roundtable.groups = groups;
+      if (this.roundtable.groups && this.roundtable.groups.length > 0) {
+        this.findRounds();
+      }
+    });
+  }
+
+  findRounds() {
+    this.seasonService.findRounds(
+      this.roundtable.selectedSeason.id, this.roundtable.selectedGroup.id).subscribe((rounds: Rest.RoundJson[]) =>
+        this.roundtable.rounds = rounds
+      );
+  }
+
+  findRoundAndTable() {
+
   }
 
   seasonSelected(event) {
@@ -51,14 +68,32 @@ export class SeasonComponent implements OnInit {
 
     let selectedSeason = this.roundtable
                              .seasons
-                             .find(season => season.id = event.target.value);
+                             .find(season => season.id == event.target.value);
 
     this.roundtable.selectedSeason = selectedSeason;
-    this.findGroup();
+    this.findGroups();
   }
 
   groupSelected(event) {
-    console.info('Selected group id: ' + event.target.value);    
+    console.info('Selected group id: ' + event.target.value);
+
+    let selectedGroup = this.roundtable
+                            .groups
+                            .find(group => group.id == event.target.value);
+
+    this.roundtable.selectedGroup = selectedGroup;
+    this.findRounds();
+  }
+
+  roundSelected(event) {
+    console.info('Selected round id: ' + event.target.value);
+
+    let selectedRound = this.roundtable
+                            .rounds
+                            .find(round => round.id == event.target.value);
+
+    this.roundtable.selectedRound = selectedRound;
+    this.findRoundAndTable();
   }
 
   /*
