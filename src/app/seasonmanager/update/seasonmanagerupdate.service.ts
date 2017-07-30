146bf67f80a027b8789ab5f9353bc0de21248567
Andre Winkler <gluehloch@googlemail.com>
2017-07-30 13:03:12 +0200

    UPDATE: Add database id

--- src/app/seasonmanager/update/seasonmanagerupdate.service.ts
+++ src/app/seasonmanager/update/seasonmanagerupdate.service.ts
@@ -1,9 +1,10 @@
 import { Injectable } from '@angular/core';
 import { RequestOptions, Headers, Http, Response } from '@angular/http';
 
+import { Observable } from 'rxjs';
+
 // TODO Was ist das hier?
 import 'rxjs/add/operator/toPromise';
-import { Observable } from 'rxjs';
 import 'rxjs/Rx';
 
 import { BetofficeService } from '../../betoffice.service';
@@ -15,19 +16,16 @@ export class SeasonManagerUpdateService extends BetofficeService {
     super(http);
   }
 
-  findSeason(seasonId: number) : Observable<Rest.SeasonJson> {
-    let response = this.http.get(this.adminUrl + 'season/' + seasonId, this.createRequestOptions());
+  findSeason(seasonId: number): Observable<Rest.SeasonJson> {
+    const response = this.http.get(this.adminUrl + 'season/' + seasonId, this.createRequestOptions());
     return response.map((r: Response) => r.json() as Array<Rest.SeasonJson>)
                    .catch(this.handleError);
   }
 
-  updateSeason(season: Rest.SeasonJson) : Observable<Rest.SeasonJson> {
-    /*
-    let response = this.http.post(this.adminUrl + 'user/update', party, this.createRequestOptions());
-    return response.map((r: Response) => r.json() as Rest.PartyJson)
+  updateSeason(season: Rest.SeasonJson): Observable<Rest.SeasonJson> {
+    const response = this.http.post(this.adminUrl + 'season/update', season, this.createRequestOptions());
+    return response.map((r: Response) => r.json() as Rest.SeasonJson)
                    .catch(this.handleError);
-    */
-    return null;
   }
 
 }
