c80280457be05dca1af9e50537ad7ac8976a2284
Andre Winkler <andre.winkler@web.de>
2018-09-01 08:20:56 +0200

    UPDATE: Moved Next/last buttons after the submit button.

--- src/app/tipp/tipp-common.component.ts
+++ src/app/tipp/tipp-common.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnChanges, SimpleChanges } from '@angular/core';
 import { HttpErrorResponse } from '@angular/common/http';
 import { CookieService } from 'ngx-cookie';
 
@@ -9,139 +9,144 @@ import { NavigationRouterService } from '../navigationrouter.service';
 import { environment } from '../../environments/environment';
 
 export class SubmitButtonModel {
-  pressed: boolean;
-  responseStatusCode: number; // HTTP Status Code 200 ok, > 400 problems
-  responseErrorMessage: String; // An error message
-  progress: number;
+    pressed: boolean;
+    responseStatusCode: number; // HTTP Status Code 200 ok, > 400 problems
+    responseErrorMessage: String; // An error message
+    progress: number;
 }
 
 export class TippModel {
-  nickname: string;
-  authenticated: boolean;
-  round: Rest.RoundJson;
-  points: number;
+    nickname: string;
+    authenticated: boolean;
+    round: Rest.RoundJson;
+    points: number;
 
-  calcPoints() {
-    this.points = 0;
-    for (const game of this.round.games) {
-      this.points = this.points + game.tipps[0].points;
+    calcPoints() {
+        this.points = 0;
+        for (const game of this.round.games) {
+            this.points = this.points + game.tipps[0].points;
+        }
     }
-  }
 }
 
-export class TippCommonComponent implements OnInit {
+export class TippCommonComponent implements OnInit, OnChanges {
 
-  currentSeasonId = environment.currentSeasonId;
+    currentSeasonId = environment.currentSeasonId;
 
-  tippModel: TippModel;
-  submitButtonModel: SubmitButtonModel;
-  navigationRouterService: NavigationRouterService;
+    tippModel: TippModel;
+    submitButtonModel: SubmitButtonModel;
+    navigationRouterService: NavigationRouterService;
 
-  constructor(private cookieService: CookieService, private tippService: TippService, navigationRouterService: NavigationRouterService) {
-    this.tippModel = new TippModel();
-    this.submitButtonModel = new SubmitButtonModel();
-    this.submitButtonModel.progress = 0;
-    this.navigationRouterService = navigationRouterService;
-  }
-
-  sortGames(games: Rest.GameJson[]): Rest.GameJson[] {
-    return games.sort((g1, g2) => {
-      const date1 = new Date(g1.dateTime);
-      const date2 = new Date(g2.dateTime);
-      return date1.getTime() - date2.getTime();
-    });
-  }
-
-  checkAuthorization() {
-    if (this.tippService.isAuthorized()) {
-      this.tippModel.nickname = this.tippService.readCredentials().nickname;
-      this.tippModel.authenticated = true;
-    } else {
-      this.tippModel.nickname = null;
-      this.tippModel.authenticated = false;
+    constructor(private cookieService: CookieService, private tippService: TippService, navigationRouterService: NavigationRouterService) {
+        this.tippModel = new TippModel();
+        this.submitButtonModel = new SubmitButtonModel();
+        this.submitButtonModel.progress = 0;
+        this.navigationRouterService = navigationRouterService;
     }
-  }
 
-  ngOnInit() {
-    this.navigationRouterService.activate(NavigationRouterService.ROUTE_TIPP);
-    this.checkAuthorization();
-
-    if (this.tippModel.authenticated) {
-      this.tippService.nextTippRound(this.currentSeasonId, this.tippModel.nickname)
-                      .subscribe((roundJson: Rest.RoundJson) => {
-                          roundJson.games = this.sortGames(roundJson.games);
-                          this.tippModel.round = roundJson;
-                          this.tippModel.calcPoints();
-                        });
+    sortGames(games: Rest.GameJson[]): Rest.GameJson[] {
+        return games.sort((g1, g2) => {
+            const date1 = new Date(g1.dateTime);
+            const date2 = new Date(g2.dateTime);
+            return date1.getTime() - date2.getTime();
+        });
     }
-  }
 
-  next() {
-    this.tippService.nextRound(this.tippModel.round.id, this.tippModel.nickname)
-                    .subscribe((roundJson: Rest.RoundJson) => {
-                        roundJson.games = this.sortGames(roundJson.games);
-                        this.tippModel.round = roundJson;
-                        this.tippModel.calcPoints();
-                        this.submitButtonModel.responseStatusCode = 0;
-                    });
-  }
+    checkAuthorization() {
+        if (this.tippService.isAuthorized()) {
+            this.tippModel.nickname = this.tippService.readCredentials().nickname;
+            this.tippModel.authenticated = true;
+        } else {
+            this.tippModel.nickname = null;
+            this.tippModel.authenticated = false;
+        }
+    }
 
-  last() {
-    this.tippService.prevRound(this.tippModel.round.id, this.tippModel.nickname)
-                    .subscribe((roundJson: Rest.RoundJson) => {
-                        roundJson.games = this.sortGames(roundJson.games);
-                        this.tippModel.round = roundJson;
-                        this.tippModel.calcPoints();
-                        this.submitButtonModel.responseStatusCode = 0;
-                    });
-  }
+    ngOnInit() {
+        this.navigationRouterService.activate(NavigationRouterService.ROUTE_TIPP);
+        this.checkAuthorization();
 
-  submitTipp() {
-    this.submitButtonModel.pressed = true;
-    this.submitButtonModel.responseStatusCode = 0;
-    this.submitButtonModel.responseErrorMessage = '';
-    this.submitButtonModel.progress = 33;
+        if (this.tippModel.authenticated) {
+            this.tippService.nextTippRound(this.currentSeasonId, this.tippModel.nickname)
+                .subscribe((roundJson: Rest.RoundJson) => {
+                    roundJson.games = this.sortGames(roundJson.games);
+                    this.tippModel.round = roundJson;
+                    this.tippModel.calcPoints();
+                });
+        }
+    }
 
-    const submitTipp = {
-      nickname: this.tippModel.nickname,
-      roundId: this.tippModel.round.id,
-      submitTippGames: []
-    };
+    ngOnChanges(changes: SimpleChanges) {
+        console.dir(changes);
+    }
 
-    this.tippModel.round.games.forEach(game => {
-      submitTipp.submitTippGames.push({
-        gameId: game.id, tippResult: {
-          homeGoals: game.tipps[0].tipp.homeGoals,
-          guestGoals: game.tipps[0].tipp.guestGoals
-        }}
-      );
-    });
+    next() {
+        this.tippService.nextRound(this.tippModel.round.id, this.tippModel.nickname)
+            .subscribe((roundJson: Rest.RoundJson) => {
+                roundJson.games = this.sortGames(roundJson.games);
+                this.tippModel.round = roundJson;
+                this.tippModel.calcPoints();
+                this.submitButtonModel.responseStatusCode = 0;
+            });
+    }
 
-    this.tippService.tipp(submitTipp)
-                    .subscribe((roundJson: Rest.RoundJson) => {
-                         roundJson.games = this.sortGames(roundJson.games);
-                         this.tippModel.round = roundJson;
-                         this.submitButtonModel.pressed = false;
-                         this.submitButtonModel.responseStatusCode = 200;
-                         this.submitButtonModel.progress = 100;
-                    },
-                    (err: HttpErrorResponse) => {
-                      this.submitButtonModel.pressed = false;
-                      this.submitButtonModel.responseStatusCode = err.status;
-                      this.submitButtonModel.responseErrorMessage = err.error;
-                      this.submitButtonModel.progress = 100;
-                      if (err.status == 403) {
+    last() {
+        this.tippService.prevRound(this.tippModel.round.id, this.tippModel.nickname)
+            .subscribe((roundJson: Rest.RoundJson) => {
+                roundJson.games = this.sortGames(roundJson.games);
+                this.tippModel.round = roundJson;
+                this.tippModel.calcPoints();
+                this.submitButtonModel.responseStatusCode = 0;
+            });
+    }
+
+    submitTipp() {
+        this.submitButtonModel.pressed = true;
+        this.submitButtonModel.responseStatusCode = 0;
+        this.submitButtonModel.responseErrorMessage = '';
+        this.submitButtonModel.progress = 33;
+
+        const submitTipp = {
+            nickname: this.tippModel.nickname,
+            roundId: this.tippModel.round.id,
+            submitTippGames: []
+        };
+
+        this.tippModel.round.games.forEach(game => {
+            submitTipp.submitTippGames.push({
+                gameId: game.id, tippResult: {
+                    homeGoals: game.tipps[0].tipp.homeGoals,
+                    guestGoals: game.tipps[0].tipp.guestGoals
+                }
+            }
+            );
+        });
+
+        this.tippService.tipp(submitTipp)
+            .subscribe((roundJson: Rest.RoundJson) => {
+                roundJson.games = this.sortGames(roundJson.games);
+                this.tippModel.round = roundJson;
+                this.submitButtonModel.pressed = false;
+                this.submitButtonModel.responseStatusCode = 200;
+                this.submitButtonModel.progress = 100;
+            },
+                (err: HttpErrorResponse) => {
+                    this.submitButtonModel.pressed = false;
+                    this.submitButtonModel.responseStatusCode = err.status;
+                    this.submitButtonModel.responseErrorMessage = err.error;
+                    this.submitButtonModel.progress = 100;
+                    if (err.status == 403) {
                         console.log('Access denied.');
                         this.tippService.clearCredentials();
-                      } else if (err.error instanceof Error) {
+                    } else if (err.error instanceof Error) {
                         // A client-side or network error occurred. Handle it accordingly.
                         console.log('An error occurred:', err.error.message);
-                      } else {
+                    } else {
                         // The backend returned an unsuccessful response code.
                         // The response body may contain clues as to what went wrong,
                         console.log(`Backend returned code ${err.status}, body was: ${err.error}`);
-                      }
-                    });
-  }
+                    }
+                });
+    }
 
 }
