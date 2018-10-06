45b555d4245d3919a4d21485ac50396310cab8e4
andrewinkler <andre.winkler@web.de>
2018-10-06 06:40:40 +0200

    FIX: CurrentSeasonId=28

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
     production: true,
+    currentSeasonId: 28,
     rootUrl: 'https://tippdiekistebier.de/betoffice-jweb/bo/office/',
     adminUrl: 'https://tippdiekistebier.de/betoffice-jweb/bo/chiefoperator/'
 };
