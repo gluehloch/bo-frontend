1a3d49f4c16d892f528311ff7239e4e3f2822468
andrewinkler <andre.winkler@web.de>
2020-01-02 10:26:12 +0100

    ADD: Shell script for app building.

---
+++ build.sh
@@ -0,0 +1,85 @@
+#!/bin/sh
+
+usage() {
+    echo "Usage: $0 -d projectdir -t targetdir"
+    echo -e "\t-d, --dir Defines the project directory."
+    echo -e "\t-t, --target Defines the deployment directory."
+    exit 1 # Exit script after printing help
+}
+
+while [ "$1" != "" ]; do 
+    case $1 in
+        -d | --dir )    shift
+                        DIR=$1
+                        ;;
+        -t | --target ) shift
+                        TARGET_DIR=$1
+                        ;;
+        -h | --help )   usage
+                        ;;
+        * )             usage
+    esac
+    shift
+done
+
+if [ -z "$DIR" ]
+then
+    DIR=$(pwd)
+fi
+
+echo "Start building betoffice web ..."
+if [ -z "$TARGET_DIR" ]
+then
+    echo "Target directory: Undefined. Start build without deployment."
+else
+   echo "Project directory: ${TARGET_DIR}"
+fi
+
+PATH=${DIR}/node_modules/.bin/:$PATH
+
+echo "Prepare production environment"
+ENV_BACKUP=${DIR}/src/environments/environment.ts.backup
+ENV_DEV=${DIR}/src/environments/environment.ts
+ENV_PROD=${DIR}/src/environments/environment.prod.ts
+
+DIST_TARGZ=boang.tar.gz
+
+DIST_DIR=${DIR}/dist/angularapp
+if [ -d "$DIST_DIR" ]; then
+    rm -r -f $DIST_DIR
+fi
+
+# Backup dev environment
+if [ -f "$ENV_BACKUP" ]; then
+    rm $ENV_BACKUP
+fi
+mv $ENV_DEV $ENV_BACKUP
+cp $ENV_PROD $ENV_DEV
+
+# Start NG build
+ng build
+
+# Package distribution
+cd $DIST_DIR
+tar -czf $DIST_TARGZ *
+
+# Restore dev environment
+cp $ENV_BACKUP $ENV_DEV
+rm $ENV_BACKUP
+
+# Copy to deployment target, if defined
+if [ -z "$TARGET_DIR" ]
+then
+    echo "Deployment directory is not defined."
+else
+    echo "Start deployment to directory ${TARGET_DIR}"
+    if [ -f "$TARGET_DIR/$DIST_TARGZ" ]
+    then
+        rm $TARGET_DIR/$DIST_TARGZ
+    fi
+
+    cp $DIST_DIR/$DIST_TARGZ $TARGET_DIR
+    tar -xzf $TARGET_DIR/$DIST_TARGZ -C $TARGET_DIR
+fi
+
+exit 0;
