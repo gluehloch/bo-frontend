4665cf1a61382a9b343c52aa9b3d051adc78e614
andrewinkler <andre.winkler@web.de>
2020-01-02 10:29:07 +0100

    FIX: Dev environment.

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -11,11 +11,11 @@ export const environment = {
 
     // website: 'andre-winkler.de.localhost',
     website: 'tippdiekistebier.de',
-    // cookieserviceUrl: 'http://192.168.99.101:9090/registrationservice/cookie/confirmCookie',
-    cookieserviceUrl: 'https://cookie.gluehloch.de/registrationservice/cookie/confirmCookie',
+    cookieserviceUrl: 'http://192.168.99.101:9090/registrationservice/cookie/confirmCookie',
+    // cookieserviceUrl: 'https://cookie.gluehloch.de/registrationservice/cookie/confirmCookie',
 
-    // rootUrl: 'http://192.168.99.101:8080/betoffice-jweb/bo/office/',
-    // adminUrl: 'http://192.168.99.101:8080/betoffice-jweb/bo/chiefoperator/',
+    rootUrl: 'http://192.168.99.101:8080/betoffice-jweb/bo/office/',
+    adminUrl: 'http://192.168.99.101:8080/betoffice-jweb/bo/chiefoperator/',
 
     // rootUrl: 'http://192.168.0.110:8080/betoffice-jweb/bo/office/',
     // adminUrl: 'http://192.168.0.110:8080/betoffice-jweb/bo/chiefoperator/',
@@ -25,8 +25,8 @@ export const environment = {
     // adminUrl: 'http://192.168.0.136:8080/betoffice-jweb/bo/chiefoperator/',
     // Laptop (W550s) IP Adresse: 192.168.0.110
 
-    rootUrl: 'https://tippdiekistebier.de/betoffice-jweb/bo/office/',
-    adminUrl: 'https://tippdiekistebier.de/betoffice-jweb/bo/chiefoperator/',
+    // rootUrl: 'https://tippdiekistebier.de/betoffice-jweb/bo/office/',
+    // adminUrl: 'https://tippdiekistebier.de/betoffice-jweb/bo/chiefoperator/',
     currentSeasonId: 29
 
 };
