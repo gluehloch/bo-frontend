a4d501a202b02030fa53659c0da78b0e55773094
Andre Winkler <andre.winkler@web.de>
2021-02-26 08:10:13 +0100

    Reactive tipp form selector for desktop/small/mobile.

--- src/app/tipp/selector/tipp-selector.component.ts
+++ src/app/tipp/selector/tipp-selector.component.ts
@@ -1,13 +1,16 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
+import { Router } from '@angular/router';
 
-import {FormControl, FormGroup} from '@angular/forms';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
     selector: 'app-tipp-selector',
     templateUrl: './tipp-selector.component.html',
     styleUrls: ['./tipp-selector.component.css']
 })
-export class TippSelectorComponent {
+export class TippSelectorComponent implements OnChanges {
+
+    @Input() selectionState: string;
 
     states = [
         {id: 'desktop', name: 'Desktop'},
@@ -16,12 +19,55 @@ export class TippSelectorComponent {
     ];
 
     form = new FormGroup({
-        state: new FormControl(this.states[0]),
+        state: new FormControl(/*this.states[0]*/),
     });
 
-    @Input('selectionState') selectionState: string;
-
-    constructor() {
+    constructor(private router: Router) {
     }
+
+    /**
+     * Reagiert auf Aenderungen an den Properties, die mit {@code @Input}
+     * annotiert sind.
+     *
+     * @param changes SimpleChanges
+     */
+    ngOnChanges(changes: SimpleChanges) {
+        console.log('ngOnChanges', changes);
+
+        if (changes.selectionState && changes.selectionState.currentValue) {
+            for (const st of this.states) {
+                if (st.id === changes.selectionState.currentValue) {
+                    // this.form.controls['state'].setValue(st, {onlySelf: true});
+                    // this.form.get('state').patchValue(st);
+                    // this.form.get('state').setValue(st);
+                    this.form.controls['state'].setValue(st);
+                }
+            }
+        }
+    }
+
+    /**
+     * Wird direkt aus dem HTML Template heraus aufgerufen:
+     * {@code (ngModelChange)="onModelChange($event)"}
+     *
+     * @param event Ein HTML DOM Event
+     */
+    public onModelChange(event: any): void {
+        console.log('onModelChange', event);
+
+        if (event.id === 'desktop') {
+            this.router.navigate(['./tipp']);
+        } else if (event.id === 'small') {
+            this.router.navigate(['./tipp-small']);
+        } else if (event.id === 'mobile') {
+            this.router.navigate(['./tipp-mobile']);
+        }
+    }
+
+/*
+    public clickButton(): void {
+        console.log('clickButton', this.form.controls['state'].value);
+    }
+*/
 
 }
