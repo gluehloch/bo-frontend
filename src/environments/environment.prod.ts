85d75cb0e656025576735252b5b76ab6c6045b8f
Andre Winkler <gluehloch@googlemail.com>
2021-06-07 20:57:41 +0200

    EM 2021: Prepare season.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
     // Der alte Filter mit Zeitzone: 'dd.MM.yyyy HH:mm': '+0200'
     dateTimeFormat: 'dd.MM.yyyy HH:mm',
     // TODO Wird mit der Community Edition abgeschafft...
-    currentSeasonId: 30,
+    currentSeasonId: 31,
 
     production: true,
     website: 'tippdiekistebier.de',
