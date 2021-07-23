49945410d5247967e29840a918bc5d8a7290371c
gluehloch <gluehloch@googlemail.com>
2021-07-23 15:26:19 +0200

    Bootstrap 5: Teilnehmer entfernen, hinzufuegen. Meisterschaft bearbeiten.

--- src/app/seasonmanager/create/create-season-model.ts
+++ src/app/seasonmanager/create/create-season-model.ts
@@ -2,10 +2,10 @@ import { SeasonType, TeamType } from './../../betoffice-json/betofficetype';
 
 export class CreateSeasonModel {
 
-  readonly seasonTypes: Array<string> = ['CL', 'CUP', 'EC', 'LEAGUE', 'UEFACUP', 'WC'];
-  readonly teamTypes: Array<string> = ['DFB', 'FIFA'];
+    readonly seasonTypes: Array<string> = ['CL', 'CUP', 'EC', 'LEAGUE', 'UEFACUP', 'WC'];
+    readonly teamTypes: Array<string> = ['DFB', 'FIFA'];
 
-  submitted = false;
-  season: Rest.SeasonJson;
+    submitted = false;
+    season: Rest.SeasonJson;
 
 }
