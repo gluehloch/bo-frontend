b1a7a4d1c1cbedb61a86e98a29460b5bb3f032b6
gluehloch <gluehloch@googlemail.com>
2021-08-15 15:44:20 +0200

    Teilnehmer Ranking: Historische Daten anzeigen.

--- src/app/ranking/ranking.component.ts
+++ src/app/ranking/ranking.component.ts
@@ -6,6 +6,7 @@ import { RankingService } from './ranking.service';
 import { NavigationRouterService } from '../navigationrouter.service';
 
 import { environment } from '../../environments/environment';
+import { SeasonService } from '../season/season.service';
 
 @Component({
   selector: 'app-ranking',
@@ -17,19 +18,26 @@ export class RankingComponent implements OnInit {
     currentSeasonId = environment.currentSeasonId;
     dateTimeFormat = environment.dateTimeFormat;
 
+    seasons: Rest.SeasonJson[];
+    selectedSeason: Rest.SeasonJson;
     ranking: Rest.UserTableJson;
     rankingRound: Rest.UserTableJson;
 
     constructor(
+        private seasonService: SeasonService,
         private rankingService: RankingService,
         private navigationRouterService: NavigationRouterService) {
     }
 
     ngOnInit() {
+        this.findSeasons();
+
         this.rankingService.calculate(this.currentSeasonId)
                            .subscribe((userTable: Rest.UserTableJson) => {
             this.calculatRoundRankingOnly(userTable);
             this.navigationRouterService.activate(NavigationRouterService.ROUTE_TEILNEHMER);
+
+            // TODO Die Meisterschaft in die Vorauswahl uebernehmen.
         });
     }
 
@@ -49,6 +57,22 @@ export class RankingComponent implements OnInit {
 
     findTipp(match: Rest.GameJson, user: Rest.UserJson): Rest.GameTippJson  {
         return _.find(match.tipps, (t) => { return t.nickname === user.nickname });
+    }
+
+    findSeasons() {
+        this.seasonService.findSeasons()
+                          .subscribe((seasons: Rest.SeasonJson[]) => {
+            this.seasons = seasons.sort((s1, s2) => s2.id - s1.id);
+            this.selectedSeason = seasons[0];
+        });
+    }    
+
+    seasonSelected(event) {
+        console.debug('Selected season id: ', event);
+
+        const selectedSeasonId = event.target.value;
+        const selectedSeason = this.seasons.find(season => season.id == selectedSeasonId);
+        this.selectedSeason = selectedSeason;
     }
 
     private calculatRoundRankingOnly(userTable: Rest.UserTableJson) {
