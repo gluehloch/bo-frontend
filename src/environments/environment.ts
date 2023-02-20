778f27f2ecd11083611e556a2e43e56ab74770a8
Andre Winkler <gluehloch@googlemail.com>
2023-02-20 09:30:30 +0100

    Eine neue Code Marotte: Das Tabellencodeformat.

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,30 +6,21 @@
 export const environment = {
 
     // Der alte Filter mit Zeitzone: 'dd.MM.yyyy HH:mm': '+0200'
-    dateTimeFormat: 'dd.MM.yyyy HH:mm',
+    dateTimeFormat:      'dd.MM.yyyy HH:mm',
     // TODO Wird mit der Community Edition abgeschafft...
-    currentSeasonId: 33,
+    currentSeasonId:     33,
 
-    production: false,
-    rootUrl: 'http://localhost:8080/betoffice-jweb/bo/office/',
-    authenticationUrl: 'http://localhost:8080/betoffice-jweb/bo/authentication/',
-    adminUrl: 'http://localhost:8080/betoffice-jweb/bo/chiefoperator/',
-    communityAdminUrl: 'http://localhost:8080/betoffice-jweb/bo/community-admin/',
+    production:          false,
+    rootUrl:             'http://localhost:8080/betoffice-jweb/bo/office/',
+    authenticationUrl:   'http://localhost:8080/betoffice-jweb/bo/authentication/',
+    adminUrl:            'http://localhost:8080/betoffice-jweb/bo/chiefoperator/',
+    communityAdminUrl:   'http://localhost:8080/betoffice-jweb/bo/community-admin/',
 
-    cookieserviceUrl: 'http://localhost:9090/registrationservice/cookie/confirmCookie',
+    cookieserviceUrl:    'http://localhost:9090/registrationservice/cookie/confirmCookie',
     // cookieserviceUrl: 'http://localhost:8080/registrationservice/cookie/confirmCookie',
-    registerserviceUrl: 'http://localhost:9090/registrationservice/registration/register',
+    registerserviceUrl:  'http://localhost:9090/registrationservice/registration/register',
     // registerserviceUrl: 'http://localhost:8080/registrationservice/registration/register',
 
-    // Laptop: 192.168.0.133 Mit Zugriff aus dem lokalen Netzwerk.
-    /*
-    rootUrl: 'http://192.168.0.133:8080/betoffice-jweb/bo/office/',
-    adminUrl: 'http://192.168.0.133:8080/betoffice-jweb/bo/chiefoperator/',
-    cookieserviceUrl: 'http://192.168.0.133:9090/registrationservice/cookie/confirmCookie',
-    registerserviceUrl: 'http://192.168.0.133:9090/registrationservice/registration/register',
-    authenticationUrl: 'http://192.168.0.133:8080/betoffice-jweb/bo/authentication/',
-    */
-
-    website: 'andre-winkler.de.localhost'
+    website:             'andre-winkler.de.localhost',
 
 };
