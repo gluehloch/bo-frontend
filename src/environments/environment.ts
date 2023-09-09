9ff027f671c209286c431ad73b756c82e1f6b7ea
gluehloch <gluehloch@googlemail.com>
2023-09-09 08:25:02 +0200

    environment...

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -16,11 +16,12 @@ export const environment = {
     adminUrl:            'http://localhost:8080/betoffice-jweb/bo/chiefoperator/',
     communityAdminUrl:   'http://localhost:8080/betoffice-jweb/bo/community-admin/',
 
-    // // cookieserviceUrl:    'http://localhost:9090/registrationservice/cookie/confirmCookie',
-    cookieserviceUrl:    'http://localhost:8080/registrationservice/cookie/confirmCookie',
-    cookieAssetsUrl:     'http://localhost:8080/registrationservice/assets/',
+    cookieserviceUrl:    'http://localhost:9090/registrationservice/cookie/confirmCookie',
+    // cookieserviceUrl:    'http://localhost:8080/registrationservice/cookie/confirmCookie',
+    // cookieAssetsUrl:     'http://localhost:8080/registrationservice/assets/',
+    cookieAssetsUrl:     'http://localhost:9090/registrationservice/assets/',
     // registerserviceUrl:  'http://localhost:8080/registrationservice/registration/register',
-    registerserviceUrl: 'http://localhost:8080/registrationservice/registration/register',
+    registerserviceUrl: 'http://localhost:9090/registrationservice/registration/register',
 
     website:             'andre-winkler.de.localhost',
 
