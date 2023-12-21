b976702476c5ca6bee0a463a803f150d54bf24b5
Andre Winkler <gluehloch@googlemail.com>
2023-12-21 18:04:46 +0100

    Prepare release

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
+const urlTippDieKisteBier = 'https://tippdiekistebier.de/betoffice-jweb/bo/';
+const urlRegistrationService = 'https://tippdiekistebier.de/registrationservice/';
+
 export const environment = {
     // Der alte Filter mit Zeitzone: 'dd.MM.yyyy HH:mm': '+0200'
     dateTimeFormat: 'dd.MM.yyyy HH:mm',
@@ -6,11 +9,11 @@ export const environment = {
 
     production:         true,
     website:            'tippdiekistebier.de',
-    rootUrl:            'https://tippdiekistebier.de/betoffice-jweb/bo/office/',
-    authenticationUrl:  'https://tippdiekistebier.de/betoffice-jweb/bo/authentication/',
-    adminUrl:           'https://tippdiekistebier.de/betoffice-jweb/bo/chiefoperator/',
-    communityAdminUrl:  'https://tippdiekistebier.de/betoffice-jweb/bo/community-admin/',
-    registerserviceUrl: 'https://cookie.gluehloch.de/registrationservice/registration/register',
-    cookieserviceUrl:   'https://cookie.gluehloch.de/registrationservice/cookie/confirmCookie',
-    cookieAssetsUrl:    'https://cookie.gluehloch.de/registrationservice/assets/',
+    rootUrl:            urlTippDieKisteBier + 'office/',
+    authenticationUrl:  urlTippDieKisteBier + 'authentication/',
+    adminUrl:           urlTippDieKisteBier + 'chiefoperator/',
+    communityAdminUrl:  urlTippDieKisteBier + 'community-admin/',
+    registerserviceUrl: urlRegistrationService + 'registration/register',
+    cookieserviceUrl:   urlRegistrationService + 'cookie/confirmCookie',
+    cookieAssetsUrl:    urlRegistrationService + 'assets/',
 };
