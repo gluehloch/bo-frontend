ad5c3c1025ee8d43777201f5dbc43038fd001465
Andre Winkler <gluehloch@googlemail.com>
2024-10-21 14:50:36 +0200

    WIP: Mobile tipp form. Or just another small layout.

--- src/app/tipp/tipp-mobile.component.ts
+++ src/app/tipp/tipp-mobile.component.ts
@@ -1,20 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 
+import { RouterLink, RouterLinkActive } from '@angular/router';
+import { NgIf, NgFor, DatePipe } from '@angular/common';
+
 import { TippCommonComponent } from './tipp-common.component';
 import { TippService } from './tipp.service';
 import { NavigationRouterService } from '../navigationrouter.service';
 import { SessionService } from '../session/session.service';
 import { FormsModule } from '@angular/forms';
 import { TippSelectorComponent } from './selector/tipp-selector.component';
-import { RouterLink, RouterLinkActive } from '@angular/router';
-import { NgIf, NgFor } from '@angular/common';
+import { SpinnerComponent } from '../shared/spinner/spinner.component';
 
 @Component({
     selector: 'app-tipp-mobile',
     templateUrl: './tipp-mobile.component.html',
     styleUrls: ['./tipp-mobile.component.css'],
     standalone: true,
-    imports: [NgIf, RouterLink, RouterLinkActive, TippSelectorComponent, FormsModule, NgFor]
+    imports: [DatePipe, NgFor, NgIf, RouterLink, RouterLinkActive, TippSelectorComponent, FormsModule, SpinnerComponent]
 })
 export class TippMobileComponent extends TippCommonComponent implements OnInit {
 
