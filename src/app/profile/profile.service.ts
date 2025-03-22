943a1fe9e5dcb8b844175a891d256b9fc142d759
Andre Winkler <gluehloch@googlemail.com>
2025-03-22 18:55:33 +0100

    WIP: post instead of put. Makes it sense?

--- src/app/profile/profile.service.ts
+++ src/app/profile/profile.service.ts
@@ -16,15 +16,15 @@ export class ProfileService extends BetofficeService {
     }
 
     updateProfile(profile: Rest.UserProfileJson): Observable<Rest.UserProfileJson> {
-        return this.http.put<Rest.UserProfileJson>(`${this.rootUrl}profile/${profile.nickname}`, profile);
+        return this.http.post<Rest.UserProfileJson>(`${this.rootUrl}profile/${profile.nickname}`, profile);
     }
 
     confirmUupdateProfile(nickname: string, confirmationToken: string): Observable<Rest.UserProfileJson> {
-        return this.http.put<Rest.UserProfileJson>(`${this.rootUrl}profile/${nickname}/confirm-update/${confirmationToken}`, confirmationToken);
+        return this.http.post<Rest.UserProfileJson>(`${this.rootUrl}profile/${nickname}/confirm-update/${confirmationToken}`, confirmationToken);
     }
 
     resubmitConfirmationMail(nickname: string): Observable<Rest.UserProfileJson> {
-        return this.http.put<Rest.UserProfileJson>(`${this.rootUrl}profile/${nickname}/resubmit-confirmation-mail`, {});
+        return this.http.post<Rest.UserProfileJson>(`${this.rootUrl}profile/${nickname}/resubmit-confirmation-mail`, {});
     }
 
 }
