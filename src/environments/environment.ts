47b10f1db289a05892b2a3939192644ee3f66340
Andre Winkler <gluehloch@googlemail.com>
2025-07-15 18:15:52 +0200

    Bundesliga 2025/26

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -14,7 +14,7 @@ export const environment = {
     // Der alte Filter mit Zeitzone: 'dd.MM.yyyy HH:mm': '+0200'
     dateTimeFormat:      'dd.MM.yyyy HH:mm',
     // TODO Wird mit der Community Edition abgeschafft...
-    currentSeasonId:     37,
+    currentSeasonId:     38,
 
     production:          false,
     rootUrl:             betoffice + 'office/',
