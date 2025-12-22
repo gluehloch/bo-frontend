59346fbf6edb2ec4cbad016519b90a2624b5b2f0
Andre Winkler <gluehloch@googlemail.com>
2025-12-22 08:29:18 +0100

    Fixed some warnings: Removed optional null/undefined parameter.

--- src/app/tipp/tipp-desktop.component.ts
+++ src/app/tipp/tipp-desktop.component.ts
@@ -1,21 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+import { NgClass, DatePipe } from '@angular/common';
+
+import { GameResultComponent } from '../shared/gameresult/gameresult.component';
+import { SpinnerComponent } from '../shared/spinner/spinner.component';
+import { NavigationRouterService } from '../navigationrouter.service';
+import { SessionService } from '../session/session.service';
 
 import { TippCommonComponent } from './tipp-common.component';
 import { TippService } from './tipp.service';
-import { NavigationRouterService } from '../navigationrouter.service';
-import { SessionService } from '../session/session.service';
-import { GameResultComponent } from '../shared/gameresult/gameresult.component';
-import { FormsModule } from '@angular/forms';
-import { TippSelectorComponent } from './selector/tipp-selector.component';
-import { RouterLink, RouterLinkActive } from '@angular/router';
-import { NgClass, DatePipe } from '@angular/common';
-import { SpinnerComponent } from '../shared/spinner/spinner.component';
 
 @Component({
     selector: 'app-tipp-desktop',
     templateUrl: './tipp-desktop.component.html',
     styleUrls: ['./tipp-desktop.component.css'],
-    imports: [SpinnerComponent, RouterLink, RouterLinkActive, TippSelectorComponent, FormsModule, GameResultComponent, NgClass, DatePipe],
+    imports: [SpinnerComponent, RouterLink, RouterLinkActive, FormsModule, GameResultComponent, NgClass, DatePipe],
     standalone: true,
 })
 export class TippDesktopComponent extends TippCommonComponent implements OnInit {
--- src/app/tipp/tipp-small.component.ts
+++ src/app/tipp/tipp-small.component.ts
@@ -1,22 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+import { NgClass, DatePipe } from '@angular/common';
 
-import { TippCommonComponent } from './tipp-common.component';
-import { TippService } from './tipp.service';
 import { NavigationRouterService } from '../navigationrouter.service';
 import { SessionService } from '../session/session.service';
 import { GameResultComponent } from '../shared/gameresult/gameresult.component';
-import { FormsModule } from '@angular/forms';
-import { TippSelectorComponent } from './selector/tipp-selector.component';
-import { RouterLink, RouterLinkActive } from '@angular/router';
-import { NgClass, DatePipe } from '@angular/common';
 import { SpinnerComponent } from '../shared/spinner/spinner.component';
+
+import { TippCommonComponent } from './tipp-common.component';
+import { TippService } from './tipp.service';
 
 @Component({
     selector: 'app-tipp-small',
     templateUrl: './tipp-small.component.html',
     styleUrls: ['./tipp-small.component.css'],
     standalone: true,
-    imports: [SpinnerComponent, RouterLink, RouterLinkActive, TippSelectorComponent, FormsModule, GameResultComponent, NgClass, DatePipe]
+    imports: [SpinnerComponent, RouterLink, RouterLinkActive, FormsModule, GameResultComponent, NgClass, DatePipe]
 })
 export class TippSmallComponent extends TippCommonComponent implements OnInit {
 
