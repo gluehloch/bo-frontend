a35ab5c2a91829009ab836f13ad293bb328c5727
gluehloch <gluehloch@googlemail.com>
2026-07-13 18:03:30 +0200

    WIP: Compiler fix

--- src/app/shared/seasonroundgame/SeasonGroupRoundSelectorService.ts
+++ src/app/shared/seasonroundgame/SeasonGroupRoundSelectorService.ts
@@ -50,11 +50,11 @@ export class SeasonGroupRoundSelectorService {
                 .subscribe((groups: Rest.GroupTypeJson[]) => {
 
             this.roundtable.groups.splice(0);
-            groups.filter(g => g.type === 'PRELIMINARY_ROUND').forEach(group => this.roundtable.groups.push(group));
-            groups.filter(g => g.type === 'KNOCKOUT_ROUND').forEach(group => this.roundtable.groups.push(group));
-            groups.filter(g => g.type === 'SEMI_FINAL').forEach(group => this.roundtable.groups.push(group));
-            groups.filter(g => g.type === 'FINAL').forEach(group => this.roundtable.groups.push(group));
-            groups.filter(g => g.type === 'LEAGUE').forEach(group => this.roundtable.groups.push(group));
+            groups.filter(g => g.groupTypeEnum === 'PRELIMINARY_ROUND').forEach(group => this.roundtable.groups.push(group));
+            groups.filter(g => g.groupTypeEnum === 'KNOCKOUT_ROUND').forEach(group => this.roundtable.groups.push(group));
+            groups.filter(g => g.groupTypeEnum === 'SEMI_FINAL').forEach(group => this.roundtable.groups.push(group));
+            groups.filter(g => g.groupTypeEnum === 'FINAL').forEach(group => this.roundtable.groups.push(group));
+            groups.filter(g => g.groupTypeEnum === 'LEAGUE').forEach(group => this.roundtable.groups.push(group));
 
             if (this.roundtable.groups.length > 0 && this.roundtable.selectedSeason) {
                 this.roundtable.selectedGroup = groups[0];
