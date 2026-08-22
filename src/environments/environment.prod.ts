d75312c2f398c93fff1bda2d4342a3174a7f0c38
Andre Winkler <gluehloch@googlemail.com>
2026-08-22 06:34:17 +0200

    Bundesliga 2026/2027

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -9,7 +9,7 @@ export const environment = {
     // Der alte Filter mit Zeitzone: 'dd.MM.yyyy HH:mm': '+0200'
     dateTimeFormat: 'dd.MM.yyyy HH:mm',
     // TODO Wird mit der Community Edition abgeschafft...
-    currentSeasonId: 39,
+    currentSeasonId: 40,
 
     production:         true,
     website:            'tippdiekistebier.de',
